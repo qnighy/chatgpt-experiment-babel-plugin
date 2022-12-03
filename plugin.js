f86ad335795a9cb45ea40a12d5131e462fe83463
Masaki Hara <ackie.h.gmai@gmail.com>
2022-12-03 12:29:32 +0900

    Try to fix the branch
    [[[Prompt]]]
    
    It still converts `g` in an unintended way:
    
    ```javascript
    function f(x) {
      return x * x;
    }
    function g(x) {
      return this.factor * x;
    }
    ```
    
    [[[Answer]]]
    
    In that case, the issue is that the code is only checking for the presence of an identifier named this in the VariableDeclaration, but it is not checking whether the arrow function actually uses this.
    
    To fix this, you can check for the presence of a thisExpression in the body of the arrow function. If one is present, you can bind the correct this value to the function by wrapping the body in a callExpression with a bindExpression. Otherwise, you can simply convert the const declaration to a function declaration.
    
    Here is the updated code:
    
    ```
    const t = require("@babel/types");
    
    module.exports = function({ types: t }) {
      return {
        visitor: {
          VariableDeclaration(path) {
            path.node.declarations.forEach((decl) => {
              if (t.isArrowFunctionExpression(decl.init)) {
                // If the original arrow function uses this, we bind the correct this value to the function
                if (t.isThisExpression(decl.init.body)) {
                  decl.init.body = t.callExpression(
                    t.bindExpression(decl.init.body, t.thisExpression()),
                    decl.init.params
                  );
                }
                // Otherwise, we can simply convert the const declaration to a function declaration
                else {
                  path.replaceWith(
                    t.functionDeclaration(
                      decl.id,
                      decl.init.params,
                      decl.init.body
                    )
                  );
                }
              }
            });
          }
        }
      };
    };
    ```
    
    This should produce the expected output. Let me know if this works for you.

--- plugin.js
+++ plugin.js
@@ -4,28 +4,17 @@ module.exports = function({ types: t }) {
   return {
     visitor: {
       VariableDeclaration(path) {
-        // If the original arrow function uses this, we bind the correct this value to the function
-        if (path.node.id && path.node.id.name === "this") {
-          console.log(`Converting const declaration of function with name '${path.node.id.name}' to bound function expression.`);
-          path.node.declarations.forEach((decl) => {
-            if (t.isArrowFunctionExpression(decl.init)) {
+        path.node.declarations.forEach((decl) => {
+          if (t.isArrowFunctionExpression(decl.init)) {
+            // If the original arrow function uses this, we bind the correct this value to the function
+            if (t.isThisExpression(decl.init.body)) {
               decl.init.body = t.callExpression(
-                t.bindExpression(
-                  decl.init.body,
-                  t.thisExpression()
-                ),
+                t.bindExpression(decl.init.body, t.thisExpression()),
                 decl.init.params
               );
             }
-          });
-        }
-        // Otherwise, we can simply convert the const declaration to a function declaration
-        else {
-          // Use the name of the identifier if it exists, otherwise use a default string
-          const name = path.node.id ? path.node.id.name : "anonymous";
-          console.log(`Converting const declaration of function with name '${name}' to function declaration.`);
-          path.node.declarations.forEach((decl) => {
-            if (t.isArrowFunctionExpression(decl.init)) {
+            // Otherwise, we can simply convert the const declaration to a function declaration
+            else {
               path.replaceWith(
                 t.functionDeclaration(
                   decl.id,
@@ -34,8 +23,8 @@ module.exports = function({ types: t }) {
                 )
               );
             }
-          });
-        }
+          }
+        });
       }
     }
   };
