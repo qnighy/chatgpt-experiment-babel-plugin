7a6b391e95e8981be3e55a77e890b765a6538464
Masaki Hara <ackie.h.gmai@gmail.com>
2022-12-03 11:48:21 +0900

    Add debug log
    [[[Prompt]]]
    
    Thanks for the fix. However, it still produces the same output as before.
    
    Could you fix the problem? If you need to debug it, please modify the source code so that you can get useful information (e.g. add logging statements for extra information)
    
    [[[Answer]]]
    
    Sure, here is an updated version of the code that should fix the problem. I've added some logging statements to help debug the issue.
    
    ```
    const t = require("@babel/types");
    
    module.exports = function({ types: t }) {
      return {
        visitor: {
          VariableDeclaration(path) {
            // If the original arrow function uses this, we bind the correct this value to the function
            if (path.node.id && path.node.id.name === "this") {
              path.node.declarations.forEach((decl) => {
                if (t.isArrowFunctionExpression(decl.init)) {
                  console.log(
                    `Function ${decl.id.name} uses 'this'. Binding 'this' to the function.`
                  );
                  decl.init.body = t.callExpression(
                    t.bindExpression(
                      decl.init.body,
                      t.thisExpression()
                    ),
                    decl.init.params
                  );
                } else {
                  console.log(
                    `Function ${decl.id.name} does not use 'this'. Skipping binding 'this'.`
                  );
                }
              });
            }
            // Otherwise, we can simply convert the const declaration to a function declaration
            else {
              path.node.declarations.forEach((decl) => {
                if (t.isArrowFunctionExpression(decl.init)) {
                  console.log(
                    `Converting const declaration of function ${decl.id.name} to function declaration.`
                  );
                  path.replaceWith(
                    t.functionDeclaration(
                      decl.id,
                      decl.init.params,
                      decl.init.body
                    )
                  );
                } else {
                  console.log(
                    `Declaration of ${decl.id.name} is not an arrow function. Skipping conversion to function declaration.`
                  );
                }
              });
            }
          }
        }
      };
    };
    ```
    
    Please let me know if this fixes the problem.

--- plugin.js
+++ plugin.js
@@ -4,24 +4,34 @@ module.exports = function({ types: t }) {
   return {
     visitor: {
       VariableDeclaration(path) {
-        path.node.declarations.forEach((decl) => {
-          if (t.isArrowFunctionExpression(decl.init)) {
-            // If the original arrow function uses this, we bind the correct this value to the function
-            if (t.isThisExpression(decl.init.body)) {
-              path.replaceWith(
-                t.variableDeclaration("const", [
-                  t.variableDeclarator(
-                    decl.id,
-                    t.callExpression(
-                      t.bindExpression(decl.init.body, t.thisExpression()),
-                      decl.init.params
-                    )
-                  ),
-                ])
+        // If the original arrow function uses this, we bind the correct this value to the function
+        if (path.node.id && path.node.id.name === "this") {
+          path.node.declarations.forEach((decl) => {
+            if (t.isArrowFunctionExpression(decl.init)) {
+              console.log(
+                `Function ${decl.id.name} uses 'this'. Binding 'this' to the function.`
+              );
+              decl.init.body = t.callExpression(
+                t.bindExpression(
+                  decl.init.body,
+                  t.thisExpression()
+                ),
+                decl.init.params
+              );
+            } else {
+              console.log(
+                `Function ${decl.id.name} does not use 'this'. Skipping binding 'this'.`
               );
             }
-            // Otherwise, we can simply convert the const declaration to a function declaration
-            else {
+          });
+        }
+        // Otherwise, we can simply convert the const declaration to a function declaration
+        else {
+          path.node.declarations.forEach((decl) => {
+            if (t.isArrowFunctionExpression(decl.init)) {
+              console.log(
+                `Converting const declaration of function ${decl.id.name} to function declaration.`
+              );
               path.replaceWith(
                 t.functionDeclaration(
                   decl.id,
@@ -29,10 +39,14 @@ module.exports = function({ types: t }) {
                   decl.init.body
                 )
               );
+            } else {
+              console.log(
+                `Declaration of ${decl.id.name} is not an arrow function. Skipping conversion to function declaration.`
+              );
             }
-          }
-        });
-      },
-    },
+          });
+        }
+      }
+    }
   };
 };
